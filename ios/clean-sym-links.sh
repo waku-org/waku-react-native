7c30c9d85d86631a02bd39dbb75d2a55e0cf5d0f
Richard Ramos <info@richardramos.me>
2022-05-29 10:40:42 -0400

    Add script to clean symbolic links

---
+++ ios/clean-sym-links.sh
@@ -0,0 +1,15 @@
+#!/bin/bashs
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Gowaku 
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Resources 
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simuslator/Gowaku.framework/Versions/Current 
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Headers
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Modules
+mv ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Versions/A/* ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/
+rm -rf ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Versions/A
+unlink ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Gowaku 
+unlink ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Resources 
+unlink ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Versions/Current 
+unlink ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Headers
+unlink ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Modules
+mv ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Versions/A/* ./Gowaku.xcframework/ios-arm64/Gowaku.framework/
+rm -rf ./Gowaku.xcframework/ios-arm64/Gowaku.framework/Versions/A
