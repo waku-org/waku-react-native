047f7d7dacd72bf7005a703f19fa958273bcd558
Richard Ramos <info@richardramos.me>
2022-05-29 10:42:58 -0400

    fix: remove symbolic links

--- ios/clean-sym-links.sh
+++ ios/clean-sym-links.sh
@@ -1,7 +1,7 @@
-#!/bin/bashs
+#!/bin/bash
 unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Gowaku 
 unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Resources 
-unlink ./Gowaku.xcframework/ios-arm64_x86_64-simuslator/Gowaku.framework/Versions/Current 
+unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Versions/Current 
 unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Headers
 unlink ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Modules
 mv ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/Versions/A/* ./Gowaku.xcframework/ios-arm64_x86_64-simulator/Gowaku.framework/
