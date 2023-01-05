1b26a8f5f51248f1fd3793080c66c560b42ddd99
Richard Ramos <info@richardramos.me>
2023-01-05 16:53:32 -0400

    feat: remove @waku/react-native dependency from example app

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,29 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-module.exports = getDefaultConfig(__dirname);
+const path = require('path');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+const modules = Object.keys({
+  ...pak.peerDependencies,
+});
+
+let config = getDefaultConfig(__dirname);
+config.projectRoot = __dirname;
+config.watchFolders = [root];
+
+config.resolver = {
+  blacklistRE: exclusionList(
+    modules.map(
+      (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+    )
+  ),
+  extraNodeModules: modules.reduce((acc, name) => {
+    acc[name] = path.join(__dirname, 'node_modules', name);
+    return acc;
+  }, {}),
+};
+
+module.exports = config;
